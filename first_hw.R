ec44a769a1bb5768de4be0bc8409f6653d67d939
ivargh18 <60360909+ivargh18@users.noreply.github.com>
2020-01-30 16:00:55 -0500

    Add files via upload

---
+++ first_hw.R
@@ -0,0 +1,16 @@
+2017^(1/3)
+abs(5.7-6.8)/.58
+a= 1:12
+a
+b= c(1,3,5,7,9, 11)
+b
+c=seq (1, 11, 2)
+c
+ln.a= log(a)
+ln.a
+s_sqrt<- sqrt(a)
+s_sqrt
+# the sd function finds the standard deviation of a value or set of values. 
+#if there are missing values, it will remove them.
+Name <- "ima"
+paste ("my name is", Name)
